6055bcba875b4585c4d0dfbf52b1d19ad7074714
Mel-Rocha <rochamel73@gmail.com>
2024-05-11 09:26:42 -0300

    expo funcionando, tem que rever a parte das img

---
+++ src/app/VinhoDessert.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const VinhoDessert = () => {
+  return (
+    <View>
+      <Text>Vinho Desset Screen</Text>
+    </View>
+  );
+};
+
+export default VinhoDessert;
